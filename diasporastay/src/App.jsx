2462a4f2fea22b04867adf9363a6cd7959cfe69d
Luel Tegabu <zonefree41@gmail.com>
2025-12-14 22:37:57 -0500

    Update src/App.jsx

--- diasporastay/src/App.jsx
+++ diasporastay/src/App.jsx
@@ -19,7 +19,7 @@ import Refund from "./pages/legal/Refund.jsx";
 
 
 // Booking Success
-import BookingSuccess from "./src/booking/BookingSuccess.jsx";
+import BookingSuccess from "./pages/booking/BookingSuccess.jsx";
 
 
 // Admin
