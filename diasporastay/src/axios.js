166a164761ee0ffeb58ec813a1ed7530226e2e33
Luel Tegabu <zonefree41@gmail.com>
2025-12-22 09:48:14 -0500

    Update diasporastay/src/axios.js

--- diasporastay/src/axios.js
+++ diasporastay/src/axios.js
@@ -2,14 +2,13 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
 });
 
-// Attach token automatically
 api.interceptors.request.use((config) => {
     const guestToken = localStorage.getItem("guestToken");
     const ownerToken = localStorage.getItem("ownerToken");
