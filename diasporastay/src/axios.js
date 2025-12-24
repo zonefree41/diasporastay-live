f71f6e123189de259e94027d956f870651b2714a
Luel Tegabu <zonefree41@gmail.com>
2025-12-24 02:24:09 -0500

    Fix API base URL for production

--- diasporastay/src/axios.js
+++ diasporastay/src/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
